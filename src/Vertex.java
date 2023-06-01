d36edfc930cafd9f2da4b4bf6707f31476ed9410
Nauriz <tasbula03@mail.ru>
2023-06-01 19:32:55 +0600

    added method which returns the map of adjacent vertices

--- src/Vertex.java
+++ src/Vertex.java
@@ -4,8 +4,13 @@ public class Vertex<V> {
     private V data;
     private Map<Vertex<V>, Double> adjVertices;
 
+    // Adds an adjacent vertex and its weight to the current vertex
     public void addAdjVertex(Vertex<V> dest, double weight) {
         adjVertices.put(dest, weight);
+    }
+
+    public Map<Vertex<V>, Double> getAdjVertices() {
+        return adjVertices;
     }
 
 }
