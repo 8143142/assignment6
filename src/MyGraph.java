00aab98c17461a9ad5279b81dbe8b96218b58322
Nauriz <tasbula03@mail.ru>
2023-06-01 20:26:26 +0600

    BreadthFirstSearch

--- src/MyGraph.java
+++ src/MyGraph.java
@@ -6,11 +6,31 @@ public class MyGraph<V> {
     public MyGraph() {
         graph = new HashMap<>();
     }
-
+    // Adds an edge between source and destination vertices with a given weight
     public void addEdge(Vertex source, Vertex destination, double weight){
         Edge edge = new Edge(source, destination, weight);
         source.addAdjVertex(destination, weight);
         if (!graph.containsKey(source)) graph.put(source, new ArrayList<>());
         graph.get(source).add(edge);
     }
+    public void BFS(Vertex start){
+        Set<Vertex> visited = new HashSet<>();
+        Queue<Vertex> queue = new LinkedList<>();
+
+        visited.add(start);
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            Vertex currentVertex = queue.poll();
+            System.out.print(currentVertex + " ");
+            Set<Vertex<V>> neighbors = currentVertex.getAdjVertices().keySet();
+            for (Vertex<V> neighbor : neighbors) {
+                if (!visited.contains(neighbor)) {
+                    visited.add(neighbor);
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
+
 }
